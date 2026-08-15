4da7f6689096ab11c51feff794f9b7e2a1ded637
OHM-Begin <ohm@example.com>
2026-08-15 18:29:49 +0700

    Fix WhatsApp Web linking version

--- index.js
+++ index.js
@@ -73,6 +73,10 @@ const client = new Client({
     authStrategy: new LocalAuth({
         clientId: 'clean-pallet-bot'
     }),
+    webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
+    },
     puppeteer: {
         headless: true,
         args: [
